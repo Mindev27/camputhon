aebbdb96218a4f30f65a7a3bb64708361644bc10
Mindev27 <126376329+Mindev27@users.noreply.github.com>
2024-07-21 19:18:35 +0900

    login & register complement
    로그인 가입 구현

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const { MongoClient, ObjectId } = require('mongodb');
 const methodOverride = require('method-override')
 const bcrypt = require('bcrypt')
@@ -12,6 +11,24 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
+app.use(passport.initialize())
+app.use(session({
+  secret: process.env.SECRET_PASSWORD,
+  resave : false,
+  saveUninitialized : false,
+  cookie : { maxAge : 60 * 60 * 1000 },
+  store : MongoStore.create({
+    mongoUrl : process.env.DB_URL,
+    dbName : 'forum'
+  })
+}))
+app.use(passport.session()) 
 
 
 let connectDB = require('./database.js')
@@ -65,4 +82,110 @@ app.put('/edit', async(req, res) => {
     }
 });
 
-app.use('/login', require('./route/login.js'))
+app.get('/write', async(req, res) => {
+    console.log(req.user)
+    if(!req.user) {
+        return res.redirect('/login');
+    }
+    if(req.user.isAdmin == false) {
+        res.send("쓰기 권한이 없습니다!");
+    }
+    res.render('write.ejs');
+});
+
+app.post('/newpost', async(req, res) => {
+
+    console.log(req.file.location)
+
+    try {
+        if(req.body.title == '') {
+            res.send("no title error");
+        } else {
+            await db.collection('post').insertOne({
+                title : req.body.title,
+                content : req.body.content,
+                img : req.file.location
+            })
+            res.redirect('/list');
+        }
+    } catch(e) {
+        console.log(e);
+        res.status(500).send("server error");
+    }
+});
+
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+    let result = await db.collection('user').findOne({ username : 입력한아이디})
+    if (!result) {
+      return cb(null, false, { message: '아이디 DB에 없음' })
+    }
+    
+    if (await bcrypt.compare(입력한비번, result.password)) {
+      return cb(null, result)
+    } else {
+      return cb(null, false, { message: '비번불일치' });
+    }
+}))
+
+passport.serializeUser((user, done) => {
+  console.log(user)
+  process.nextTick(() => {          
+    done(null, { id: user._id, username: user.username })
+  })
+})
+passport.deserializeUser(async(user, done)=>{
+    let result = await db.collection('user').findOne({_id: new ObjectId(user.id)})
+    delete result.password
+    process.nextTick(()=>{
+        done(null, result)
+    })
+})
+
+function checkIdPw(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        res.send("아이디 비번을 입력하세요.");
+    } else {
+        next();
+    }
+}
+
+
+app.get('/login', async(req, res) => {
+    res.render('login.ejs');
+  });
+  
+  app.post('/login', checkIdPw, async (req, res, next) => {
+    passport.authenticate('local', (error, user, info)=>{
+        if(error) return res.status(500).json(error)
+        if(!user) return res.status(401).json(info.message)
+        req.logIn(user, (err)=>{
+            if(err) return next(err)
+            res.render('mypage.ejs', { data : req.user })
+        })
+    })(req, res, next)
+});
+
+  
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkIdPw, async (req, res) => {
+    let sameId = await db.collection('user').findOne({ username: req.body.username })
+    if(sameId) {
+        return res.status(409).json({ message: '이미 사용중인 아이디입니다.' })
+    }
+
+    if(req.body.password != req.body.passwordCheck ) {
+        return res.status(400).json({ message: '비밀번호가 일치하지 않습니다.' }); 
+    }
+
+    let hashed = await bcrypt.hash(req.body.password, 10)
+    console.log(hashed)
+
+    let result = await db.collection('user').insertOne({
+        username : req.body.username,
+        password : hashed
+    })
+    res.redirect('/list')
+})
